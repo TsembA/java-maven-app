c6d3d0ebfe8334a4136059a6703f8c220f50a371
Arno Tsembenhoi <tsembenhoi@gmail.com>
2023-12-14 13:19:05 -0800

    Building and push Docker image with the Jenkins, and aading Groovi funcrions

--- script.groovy
+++ script.groovy
@@ -1,13 +1,19 @@
-def buildApp() {
-    echo 'building the application...'
+def buildJar() {
+    echo 'building the application'
+    sh 'mvn package'
 }
 
-def testApp() {
-    echo 'test the application...'
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t tsemb/demo-app:jma-2.0 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push tsemb/demo-app:jma-2.0'
+    }
 }
 
-def deployApp() {
-    echo 'deploying the application...'
-    echo "deploying version ${params.VERSION}" 
+def deployImage() {
+    echo "deploing the application..."
 }
+
 return this
