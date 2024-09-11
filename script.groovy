b57bdedfad5c9c7a7cb2ac33f1478a8c16b8fbcf
Arno Tsembenhoi <tsembenhoi@gmail.com>
2024-09-11 12:10:55 -0700

    Add groovy script with new credentials

--- script.groovy
+++ script.groovy
@@ -5,10 +5,10 @@ def buildJar() {
 
 def buildImage() {
     echo 'build and push image...'
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t tsemb/demo-app:jma-5.0 .'
+    withCredentials([usernamePassword(credentialsId: 'globla-credentials-dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t tsemb/demo-app:jma-6.1 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push tsemb/demo-app:jma-5.0'
+        sh 'docker push tsemb/demo-app:jma-6.0'
     }
 
 }
